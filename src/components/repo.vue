30eddca91edc2322b0174dec3801f050c024b9d0
Everkers <waelkourich12@gmail.com>
2019-12-06 23:25:16 +0100

    add infinityscroll

--- src/components/repo.vue
+++ src/components/repo.vue
@@ -1,26 +1,77 @@
 <template>
   <div class='repo-ur'>
+      <a :href="repoOwnerUrl" target="_blank">
       <div class="left_avatar">
-          <img src="https://cdn140.picsart.com/306598398118201.jpg?r1024x1024" alt="avatar">
+          <img :title="repoOwner" :src="repoAvatar" alt="avatar">
       </div>
+      </a>
       <div class="right_info">
           <div class="head">
-              <h3>Tenserflow</h3>
-              <span>Submitted 3 Days</span>
+              <a :href="repoUrl" style="color:inherit" target="_blank"><h3>{{repoName}}</h3></a>
+              <span>Submitted {{createdDateFromat}} By {{repoOwner}}</span>
           </div>
           <div class="description">
-              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam eos, nemo, quo qui maxime cum quaerat ipsam laborum corporis quas optio vitae eius quibusdam, est exercitationem aliquid incidunt mollitia nulla. Voluptatem aspernatur assumenda optio itaque impedit accusamus aut ipsa ipsum.</p>
+              <p>{{descriptionFormater(repoDescription)}}</p>
+              <a @click.prevent = 'more()' href="#" v-if="readMore">{{showMore ? 'Read Less' : 'Read More'}}</a>
           </div>
           <div class="stats">
-              <button class='button button--main'>Stars : 10K</button>
-              <button class='button button--main'>Issues : 10K</button>
+              <button class='button button--main'>Stars : {{toThousands(repoStars)}}</button>
+              <button class='button button--main'>Issues : {{toThousands(repoIssues)}}</button>
           </div>
       </div>
   </div>
 </template>
 
 <script>
+import moment from 'moment'
 export default {
+    props:[
+     'repoName' ,
+     'repoDescription' ,
+     'repoStars',
+     'repoIssues' ,
+     'repoAvatar' ,
+     'repoCreatedAt' ,
+     'repoOwner',
+     'repoUrl',
+     'repoOwnerUrl'
+     ],
+    data(){
+        return{
+            readMore:false,
+            showMore:false,
+            sliceLength:110
+        }
+    },
+    computed:{
+        createdDateFromat(){
+            return moment(this.$props.repoCreatedAt).fromNow()
+        }
+    },
+    methods:{
+        toThousands(num) {
+            return Math.abs(num) > 999 ? Math.sign(num)*((Math.abs(num)/1000).toFixed(1)) + 'k' : Math.sign(num)*Math.abs(num)
+        },
+        more(){
+            if(this.showMore){
+                this.sliceLength = 110
+                this.showMore = false
+            }
+            else{
+                this.sliceLength = this.$props.repoDescription.length
+                this.showMore = true
+            }
+        },
+        descriptionFormater(description){
+            const words = description.split(' ');
+            if(words.length >= 26){
+                this.readMore = true
+                return description.slice(0 , this.sliceLength) + ' '
+            }
+            return description
+        }
+    }
+
 
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="home-ur">
       <b-container class='pl-md-0'>
-          <h1 class='text-center text-sm-left' style="max-width:600px">Most startted GitHub repos  in the last 30 days</h1> 
+          <h1 v-if='!loading && !error' class='text-center text-sm-left' style="max-width:600px">Most startted GitHub repos  in the last 30 days</h1> 
+          <b-row v-if='error' class='error' >
+              <img src="../assets/images/pablo-come-back-later.png" alt="">
+              <h4>An error occurred in <br> loading this content</h4>
+          </b-row>
           <div class="repos">
               <div v-if='loading' class="fakes" v-for="fake in 12">
               <content-loader :height="190" :width="800" :speed="2"  primaryColor="#f3f3f3" secondaryColor="#ecebeb">
@@ -13,12 +17,21 @@
                     <rect x="286.27" y="139.67" rx="0" ry="0" width="60" height="28" />
               </content-loader>
               </div>
-              <div v-if='!loading' class="real">
-                <repo-ur></repo-ur>
-                <repo-ur></repo-ur>
-                <repo-ur></repo-ur>
-                <repo-ur></repo-ur>
+              <div v-if='!loading && !error' v-for='repo in repos' class="real">
+                <repo-ur :repo-name='repo.name' 
+                :repo-description='repo.description' 
+                :repo-stars='repo.stargazers_count'
+                :repo-issues='repo.open_issues'
+                :repo-avatar='repo.owner.avatar_url'
+                :repo-created-at='repo.created_at'
+                :repo-owner='repo.owner.login'
+                :repo-url='repo.html_url'
+                :repo-owner-url='repo.owner.html_url'
+                ></repo-ur>
               </div>
+
+              <infinite-loading v-if='!loading && !error' @infinite="infiniteHandler"></infinite-loading>
+
           </div>
       </b-container>
   </div>
@@ -26,19 +39,73 @@
 
 <script>
 import { ContentLoader } from 'vue-content-loader'
+import InfiniteLoading from 'vue-infinite-loading';
+import axios from 'axios'
+const api = 'https://api.github.com/search/repositories'
 export default {
     data(){
         return{
-            loading:true
+            loading:true,
+            repos:[],
+            created:'',
+            error:false,
+            page:1
+            
         }
     },
     components: {
-        ContentLoader
+        ContentLoader,
+        InfiniteLoading
+    },
+    methods:{
+         async infiniteHandler($state){
+           try{
+            const data = await axios.get(api ,
+                         {params:{
+                         q:`created:>${this.created}`,
+                         sort:'stars',
+                         order:'desc'
+                         ,per_page:27 , 
+                         page:this.page }})
+            const items = data.data.items;
+            items.forEach(item => this.repos.push(item) )
+            $state.loaded()
+            this.page++
+           }
+           catch(err){
+               this.error = true
+               console.log(err)
+           }
+        },
+        async getRepos(){
+           try{
+                const {data} = await axios.get(api ,
+                                {params:{
+                                q:`created:>${this.created}`,
+                                sort:'stars',
+                                order:'desc'
+                                ,per_page:27 , 
+                                page:this.page }})
+
+                data.items.forEach(item=>{
+                    this.repos.push(item)
+                })                
+                this.loading  = false 
+                this.page++
+           }
+           catch(err){
+               this.error = true
+               this.loading = false
+               console.error(err)
+           }
+        }
     },
     mounted(){
-        setTimeout(()=>{ //just for testing 
-            this.loading = false
-        } , 2000)
+        const date = 
+        new Date(new Date().setDate(new Date().getDate() - 30)) //get the current date - 30 days 
+        .toISOString().split('T')[0] //split to get the date on iso format only 
+        this.created = date
+        this.getRepos()
     }
 
 }
